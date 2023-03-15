e5fb11c18594b7af26c044f8172a1ee91be80515
Nawal2525 <karanstha197@gmail.com>
2023-03-15 13:20:13 +0545

    generate button, textformfield and image sizes

--- lib/ai_image_generator.dart
+++ lib/ai_image_generator.dart
@@ -1,6 +1,8 @@
-import 'package:ai_image_generator/app/ui/pages/01_homepage/homepage_screen.dart';
+import 'package:ai_image_generator/app/routes/routes.dart';
+import 'package:ai_image_generator/app/ui/theme/color_const.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AiImageGenerator extends StatelessWidget {
   const AiImageGenerator({super.key});
@@ -10,10 +12,16 @@ class AiImageGenerator extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'AI Image Generator',
       themeMode: ThemeMode.system,
-      home: const HomepageScreen(),
       theme: ThemeData(
         useMaterial3: true,
+        scaffoldBackgroundColor: bgColor,
+        fontFamily: GoogleFonts.poppins().fontFamily,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+        ),
       ),
+      getPages: Routes.appRoutes(),
     );
   }
 }
--- lib/app/ui/pages/01_homepage/homepage_screen.dart
+++ lib/app/ui/pages/01_homepage/homepage_screen.dart
@@ -1,4 +1,6 @@
+import 'package:ai_image_generator/app/ui/theme/color_const.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class HomepageScreen extends StatefulWidget {
   const HomepageScreen({super.key});
@@ -8,8 +10,106 @@ class HomepageScreen extends StatefulWidget {
 }
 
 class _HomepageScreenState extends State<HomepageScreen> {
+  var sizes = ["Small", "Medium", "Large"];
+  var value = ["256x256", "512x512", "1024x1024"];
+  String? dropValue;
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          "AI Image Generator",
+          style: TextStyle(
+            color: whiteColor,
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Container(
+                          height: 40,
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 16, vertical: 4),
+                          decoration: BoxDecoration(
+                              color: whiteColor,
+                              borderRadius: BorderRadius.circular(12)),
+                          child: TextFormField(
+                            decoration: const InputDecoration(
+                              hintText: "eg 'lion on a champ' ",
+                              border: InputBorder.none,
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 12),
+                      Container(
+                        height: 44,
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 16, vertical: 4),
+                        decoration: BoxDecoration(
+                          color: whiteColor,
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: DropdownButtonHideUnderline(
+                          child: DropdownButton(
+                            icon: const Icon(Icons.expand_circle_down_outlined,
+                                color: btnColor),
+                            hint: const Text("Select Size"),
+                            items: List.generate(
+                              sizes.length,
+                              (index) => DropdownMenuItem(
+                                value: value[index],
+                                child: Text(
+                                  sizes[index],
+                                ),
+                              ),
+                            ),
+                            onChanged: (value) {},
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    width: Get.width,
+                    height: 45,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: btnColor,
+                        shape: const StadiumBorder(),
+                      ),
+                      child: const Text(
+                        "Generate",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 4,
+              child: Container(
+                color: Colors.amber,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
